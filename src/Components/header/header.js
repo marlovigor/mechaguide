202eb9da1972cd03d0a0b8af37f3ffeb313d1f5f
Marlo Jones <marlojones@pop-os.localdomain>
2021-06-17 14:22:45 -0400

    prgress3

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState }  from "react";
 import './header.css'
-// import Banner from "../../img/banners"
+import Carousel from 'react-material-ui-carousel'
 function Header() {
+
+  const [ arr, changarr] = useState([
+
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!"
+  },
+  {
+      name: "Random Name #2",
+      description: "Hello World!"
+  }
+  ])
+
+
   return (
+    
     <div>
       <img className="main-image" src="/my_logo.png" width="100%" height="20%"/>
+       <Carousel>
+            {arr.map( (item)=>{return <h1>{item.name}</h1>})}
+        </Carousel>
+      {/* <img className="main-image" src="/my_logo.png" width="100%" height="20%"/> */}
     </div>
   );
 }
